d47e7926bc12f88d335a67933860611b9fad77a9
RomanoBoldrin <vitorromanoboldrin@hotmail.com>
2025-12-23 23:44:23 -0300

    Add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
-function Home()
-{
-    return <h1>Oi pai, oi mãe. Amo vocês. Estou fazendo meu primeiro projeto na internet!!!</h1>
+function Home() {
+  return (
+    <h1>
+      Oi pai, oi mãe. Amo vocês. Estou fazendo meu primeiro projeto na
+      internet!!!
+    </h1>
+  );
+}
+
+function test() {
+  console.log("Wrong indentation!!!\n");
 }
 
 export default Home;
